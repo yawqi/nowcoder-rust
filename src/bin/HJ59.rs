f6ad8e004996b0c98c09d5800588c4d9b87c9098
Qi Wang <wangqi@linux.alibaba.com>
2022-07-02 20:29:19 +0800

    Finish HJ59

---
+++ src/bin/HJ59.rs
@@ -0,0 +1,30 @@
+use std::collections::{HashSet, HashMap};
+fn main() {
+    let mut s = String::new();
+    std::io::stdin()
+        .read_line(&mut s);
+    let mut st = HashSet::new();
+    let mut once = HashMap::new();
+    s.trim().chars().enumerate().for_each(|(i, c)| {
+        if st.contains(&c) {
+            once.remove(&c);
+        } else {
+            st.insert(c);
+            once.insert(c, i);
+        }
+    });
+
+    if once.is_empty() {
+        println!("-1");
+    } else {
+        let mut idx = usize::MAX;
+        let mut c = ' ';
+        for (ch, i) in once.into_iter() {
+            if i < idx {
+                idx = i;
+                c = ch;
+            }
+        }
+        println!("{}", c);
+    }
+}
